4f6da239feca6cdde83a16259129342cbf0bafe6
jennystilley <jenny@shoppressie.com>
2025-05-11 09:30:37 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 import pkg from 'pg'
 const { Pool } = pkg
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+})
 
 export default {
   async getCustomer(email) {
